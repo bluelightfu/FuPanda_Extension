6883245da0fbbbd0c19ad492e6d0bdab79bacdaf
Shao Fu Chen <abcdmarcus@gmail.com>
2023-03-13 15:29:36 +0800

    完成一版自動填寫JCB星巴克

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("how2know1").click();
+document.getElementById("user_name").value = "陳少甫";
+document.getElementById("user_phone").value = "0927989721";
+document.getElementById("card1").value = "356964";
+document.getElementById("card4").value = "3109";
+document.getElementById("user_email").value = "abcdmarcus@gmail.com";
+document.getElementById("CheckboxGroup1_0").click();
+let captcha = document.getElementById("checkCode").innerHTML;
+document.getElementById("captcha_form").value = captcha;
